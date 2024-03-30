4dbc49751dbd7931bd6b88f2322b6a674208d409
Layvu <zemskyalexey.writer@mail.ru>
2024-03-30 23:34:29 +0500

    The main app is ready, pages index and about

--- app1/main/views.py
+++ app1/main/views.py
@@ -4,14 +4,17 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Floral',
-        'content': 'Главная страница магазина - Floral',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_authenticated': False
+        'title': 'Floral - Главная',
+        'content': "Комнатные растения и цветы"
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'Floral - О нас',
+        'content': "О нас",
+        'text_on_page': "Челеееееееееееееееен"
+    }
+
+    return render(request, 'main/about.html', context)
