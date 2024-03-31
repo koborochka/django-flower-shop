f44ea97e7a802accc3a975e1f37d46f0d1e9cdde
Layvu <zemskyalexey.writer@mail.ru>
2024-03-31 20:41:14 +0500

    After adding fixtures for goods

--- app1/goods/views.py
+++ app1/goods/views.py
@@ -2,7 +2,43 @@ from django.shortcuts import render
 
 
 def catalog(request):
-    return render(request, 'goods/catalog.html')
+    context = {
+        "title": "Floral - Каталог",
+        "goods": [
+            {
+                "image": "deps/img/goods/kalanchoe_1.png",
+                "article": "529360177",
+                "name": "Каланхоэ каландива микс",
+                "price": 650,
+            },
+            {
+                "image": "deps/img/goods/kalanchoe_2.png",
+                "article": "126383293",
+                "name": "Каланхоэ махровый микс",
+                "price": 585,
+            },
+            {
+                "image": "deps/img/goods/kalanchoe_2.png",
+                "article": "126383293",
+                "name": "Каланхоэ махровый микс",
+                "price": 585,
+            },
+            {
+                "image": "deps/img/goods/kalanchoe_2.png",
+                "article": "126383293",
+                "name": "Каланхоэ махровый микс",
+                "price": 585,
+            },
+            {
+                "image": "deps/img/goods/kalanchoe_2.png",
+                "article": "126383293",
+                "name": "Каланхоэ махровый микс",
+                "price": 585,
+            },
+        ],
+    }
+    return render(request, "goods/catalog.html", context)
+
 
 def product(request):
-    return render(request, 'goods/product.html')
+    return render(request, "goods/product.html")
--- app1/main/views.py
+++ app1/main/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     context = {
         'title': 'Floral - Главная',
-        'content': "Комнатные растения и цветы"
+        'content': "Комнатные растения и цветы",
+        'categories': categories
     }
 
     return render(request, 'main/index.html', context)
