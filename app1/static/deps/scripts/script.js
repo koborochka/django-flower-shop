5df42c9190724f1e3164c1a3148e1eced8c3c67c
koborochka <116395397+koborochka@users.noreply.github.com>
2024-04-03 22:59:35 +0500

    Filters and card layout (#8)
    * remaned troubled files, layout filters
    
    * styles for catalog filters

--- app1/static/deps/scripts/script.js
+++ app1/static/deps/scripts/script.js
@@ -1,7 +1,6 @@
 let personalCabinetButton = document.querySelector('.personal-cabinet__button');
 let personalCabinetMenu = document.querySelector('.main-navigation__dropdown-menu');
 personalCabinetButton.onclick = function() {
-    console.log("d4lr")
     personalCabinetMenu.classList.toggle('visually-hidden');
     personalCabinetMenu.classList.toggle('visually-shown');
 
--- deps/scripts/script.js
+++
@@ -1,8 +0,0 @@
-let personalCabinetButton = document.querySelector('.personal-cabinet__button');
-let personalCabinetMenu = document.querySelector('.main-navigation__dropdown-menu');
-personalCabinetButton.onclick = function() {
-    console.log("d4lr")
-    personalCabinetMenu.classList.toggle('visually-hidden');
-    personalCabinetMenu.classList.toggle('visually-shown');
-
-};
