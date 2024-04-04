6803dd703c94f005871a18ca06d48405bfd6c3ce
Layvu <zemskyalexey.writer@mail.ru>
2024-04-04 13:33:57 +0500

    The product filter on catalog page

--- app1/goods/views.py
+++ app1/goods/views.py
@@ -1,5 +1,4 @@
-from re import A
-from unicodedata import category
+from urllib import request
 from django.shortcuts import get_list_or_404, render
 
 from goods.models import Products
@@ -7,16 +6,28 @@ from goods.models import Products
 
 def catalog(request, category_slug):
 
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+
     if category_slug == 'all':
         goods = Products.objects.all()
     elif category_slug == 'sales':
         goods = Products.objects.filter(discount__gt=0)
     else:
-        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+        goods = Products.objects.filter(category__slug=category_slug)
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
+
+    goods = get_list_or_404(goods);
 
     context = {
         "title": "Floral - Каталог",
         "goods": goods,
+        "slug_url": category_slug
     }
     return render(request, "goods/catalog.html", context)
 
