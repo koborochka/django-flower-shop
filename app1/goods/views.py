ca4f4c049d1b3ef7c0d7fdff2ddaf3f384e6047d
Layvu <zemskyalexey.writer@mail.ru>
2024-04-04 18:46:36 +0500

    Simple search created, product images uploaded

--- app1/goods/views.py
+++ app1/goods/views.py
@@ -2,15 +2,19 @@ from urllib import request
 from django.shortcuts import get_list_or_404, render
 
 from goods.models import Products
+from goods.utils import q_search
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
 
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     if category_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     elif category_slug == 'sales':
         goods = Products.objects.filter(discount__gt=0)
     else:
