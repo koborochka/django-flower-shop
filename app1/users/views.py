f3ea41f6bda853a414808dd9e0116b36763797b5
Layvu <zemskyalexey.writer@mail.ru>
2024-04-21 17:07:20 +0500

    Registration form and controller are ready

--- app1/users/views.py
+++ app1/users/views.py
@@ -1,9 +1,9 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -25,13 +25,23 @@ def login(request):
     }
     return render(request, 'users/login.html', context)
 
-
 def registration(request):
-    context = {
-        'title': 'Floral - Регистрация'
-    }
-    return render(request, 'users/registration.html', context)
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
 
+    context = {
+        'title': 'Floral - Регистрация',
+        'form': form
+    }
+
+    return render(request, 'users/registration.html', context)
 
 def profile(request):
     context = {
@@ -41,4 +51,5 @@ def profile(request):
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
