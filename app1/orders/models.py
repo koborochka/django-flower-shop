a62320aba2ce8744bba25c10649b169b172a60d9
koborochka <098mishawifi@gmail.com>
2024-05-28 21:56:47 +0500

    переделал страницу "о нас", подправил верстку

--- app1/orders/models.py
+++ app1/orders/models.py
@@ -34,13 +34,16 @@ class Order(models.Model):
     def __str__(self):
         return f"Заказ № {self.pk} | Покупатель {self.user.first_name} {self.user.last_name}"
 
+    def total_price(self):
+        return sum(item.products_price() for item in self.orderitem_set.all())
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(to=Order, on_delete=models.CASCADE, verbose_name="Заказ")
     product = models.ForeignKey(to=Products, on_delete=models.SET_DEFAULT, null=True, verbose_name="Продукт",
                                 default=None)
     name = models.CharField(max_length=150, verbose_name="Название")
-    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Цена")
+    price = models.PositiveIntegerField(default=0,  verbose_name="Цена")
     quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
     created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Дата продажи")
 
@@ -52,7 +55,8 @@ class OrderItem(models.Model):
     objects = OrderitemQueryset.as_manager()
 
     def products_price(self):
-        return round(self.price() * self.quantity, 2)
+        return round(self.price * self.quantity, 0)
 
     def __str__(self):
         return f"Товар {self.name} | Заказ № {self.order.pk}"
+
