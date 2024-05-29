190f792e1a2ab7afc7a81a296551b927ed554f28
koborochka <098mishawifi@gmail.com>
2024-05-29 21:22:57 +0500

    изменил путь к static, media

--- app1/app/settings.py
+++ app1/app/settings.py
@@ -139,15 +139,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    BASE_DIR / "static"
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
-MEDIA_URL = 'media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "staticfiles/deps/pictures")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "staticfiles/deps/pictures"
 
 INTERNAL_IPS = [
     # ...
