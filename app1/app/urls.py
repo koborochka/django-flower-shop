c2e05dd74063d113d6133ba473c5c217357bd0c5
koborochka <098mishawifi@gmail.com>
2024-06-15 17:55:40 +0500

    подправил файл urls.py

--- app1/app/urls.py
+++ app1/app/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf.urls.static import static
-
+from django.views.static import serve
 from app import settings
 
 urlpatterns = [
@@ -34,3 +34,7 @@ if settings.DEBUG:
         path("__debug__/", include("debug_toolbar.urls")),
     ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
