79b47acd54f64c30706a5f09f4f00f6ce993cd90
koborochka <098mishawifi@gmail.com>
2024-06-15 19:21:22 +0500

    убрал staticfiles_storage

--- app1/app/settings.py
+++ app1/app/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 MEDIA_URL = '/media/'
